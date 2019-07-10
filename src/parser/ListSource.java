9e6142ac9da558b9d6371f6828231a91224eae86
masao <masao@Maverick>
2019-07-10 21:42:38 +0900

    Source which uses List as source.

---
+++ src/parser/ListSource.java
@@ -0,0 +1,74 @@
+/*
+ Copyright 2017, 2018 Masao Tomono
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package parser;
+
+import java.util.List;
+
+/**
+ *
+ * @author masao
+ * @param <T>
+ */
+public class ListSource<T> extends Source<List<T>, T> {
+    
+    public ListSource(List<T> s) {
+        super(s);
+    }
+    
+    @Override
+    public ListSource clone() {
+        ListSource<T> ret = new ListSource<>(src);
+        ret.pos = pos;
+        return ret;
+    }
+    
+    @Override
+    public final T peek() throws ParseException {
+        if (pos >= src.size())
+            throw new ParseException("unexpectedly reached end of text.");
+        return src.get(pos);
+    }
+    
+    @Override
+    public final String explain(String e) {
+        String ret = super.explain(e);
+        if (src!=null && 0<=pos && pos<src.size()) 
+            ret += ": '" + src.get(pos) + "'";
+        return ret;
+    }
+
+    @Override
+    public List<T> sub(int... sec) {
+        return src.subList(sec[0], sec[1]);
+    }
+
+    @Override
+    public T get(int at) throws ParseException {
+        if (src.size()<=pos)
+            throw new ParseException("unexpectedly reached end of text.");
+        return src.get(at);
+    }
+    
+    @Override
+    public List<T> rest() {
+        return src.subList(pos,src.size());
+    }
+    
+    @Override
+    public void forward(int jump) {
+        this.pos = this.pos+jump;
+    }
+}
