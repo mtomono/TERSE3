2cb224f3e39873227b5755f3de9523a2a6957d66
masao <masao@Maverick>
2020-05-13 20:13:22 +0900

    added RangeUtil#inEitherWay()

--- test/orderedSet/RangeUtilNGTest.java
+++ test/orderedSet/RangeUtilNGTest.java
@@ -7,6 +7,7 @@ package orderedSet;
 
 import collection.TList;
 import java.util.Optional;
+import static orderedSet.RangeUtil.inEitherWay;
 import static orderedSet.RangeUtil.intersectRI;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
@@ -48,6 +49,26 @@ public class RangeUtilNGTest {
         TList<Range<Integer>> tested = TList.sof();
         Optional<Range<Integer>> result = intersectRI(tested);
         Optional<Range<Integer>> expected = Optional.empty();
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testInEitherWay() {
+        System.out.println(test.TestUtils.methodName(0));
+        Range<Integer> result = inEitherWay(0,1);
+        Range<Integer> expected = new Range<>(0,1);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testInEitherWayR() {
+        System.out.println(test.TestUtils.methodName(0));
+        Range<Integer> result = inEitherWay(1,0);
+        Range<Integer> expected = new Range<>(0,1);
         System.out.println("result  : " + result);
         System.out.println("expected: " + expected);
         assertEquals(result, expected);
