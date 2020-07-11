f9b32efa1f9c6b513b5d006ad4bbb92ae0e0027f
masao <masao@Maverick>
2020-07-11 19:09:14 +0900

    tested green.

--- test/collection/label/HierarchyNGTest.java
+++ test/collection/label/HierarchyNGTest.java
@@ -6,6 +6,7 @@
 package collection.label;
 
 import collection.P;
+import collection.TList;
 import static collection.label.Hierarchy.create;
 import debug.Te;
 import static org.testng.Assert.*;
@@ -23,7 +24,8 @@ public class HierarchyNGTest {
     @Test
     public void testPut() {
         System.out.println(test.TestUtils.methodName(0));
-        Hierarchy<String> tested = create(0,(h,t)->Te.e(P.p(h,t)));
+        TList<P<TList<Integer>,String>> list=TList.c();
+        Hierarchy<String> tested = create(0,(h,t)->list.add(Te.e(P.p(h.hierarchy,t))));
         tested.down()
               .put("_00000")
               .put("_00001")
@@ -38,8 +40,19 @@ public class HierarchyNGTest {
               .put("_00007")
               .put("_00008")
               .put("_00009");
-        int result = 0;
-        int expected = 0;
+        TList<P<TList<Integer>,String>> result = list;
+        TList<P<TList<Integer>,String>> expected = TList.sof(
+                P.p(TList.sof(0),"_00000"),
+                P.p(TList.sof(1),"_00001"),
+                P.p(TList.sof(2),"_00002"),
+                P.p(TList.sof(2,0),"_00003"),
+                P.p(TList.sof(2,1),"_00004"),
+                P.p(TList.sof(2,1,0),"_00005"),
+                P.p(TList.sof(2,1,1),"_00006"),
+                P.p(TList.sof(2,2),"_00007"),
+                P.p(TList.sof(2,3),"_00008"),
+                P.p(TList.sof(2,4),"_00009")
+        );
         System.out.println("result  : " + result);
         System.out.println("expected: " + expected);
         assertEquals(result, expected);
