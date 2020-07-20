fb9c7f3188b4c8ae48243542917165ce72704233
masao <masao@Maverick>
2020-07-20 20:57:57 +0900

    improved readability, memory consumption and performance of Knapsack by KnapsackLine. tested green.

--- test/solver/KnapsackNGTest.java
+++ test/solver/KnapsackNGTest.java
@@ -6,6 +6,7 @@
 package solver;
 
 import collection.TList;
+import debug.Te;
 import java.util.TreeMap;
 import orderedSet.Comparators;
 import static org.testng.Assert.*;
@@ -198,6 +199,61 @@ public class KnapsackNGTest {
         assertEquals(result, expected);
     }
 
+    @Test(enabled=false)
+    public void testKnapsackLarge() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Luggage> tested = TList.of(
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8)
+        );
+        
+        int result = new Knapsack().target(Solvers.extract2i(tested,Luggage::weight,Luggage::value)).solve(60).value;
+        int expected = 95;
+        System.out.println("result  : "+result);
+        System.out.println("expected: "+expected);
+        assertEquals(result, expected);
+    }
+
     @Test
     public void testKnapsackMemo() {
         System.out.println(test.TestUtils.methodName(0));
@@ -326,8 +382,210 @@ public class KnapsackNGTest {
                 new Luggage(5,8)
         );
         
-        Integer result = KnapsackFlat.solve(tested.map(l->p2i(l.weight(),l.value())),15).get(0,15);
+        Integer result = Te.e(KnapsackFlat.solve(tested.map(l->p2i(l.weight(),l.value())),15)).get(0,15);
         Integer expected = 20;
+        System.out.println("result  : "+result);
+        System.out.println("expected: "+expected);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testKnapsackFlatLarge() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Luggage> tested = TList.of(
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8)
+        );
+        
+        int result = Te.e(KnapsackFlat.solve(tested.map(l->p2i(l.weight(),l.value())),60)).get(0,60);
+        int expected = 95;
+        System.out.println("result  : "+result);
+        System.out.println("expected: "+expected);
+        assertEquals(result, expected);
+    }
+    @Test
+    public void testKnapsackLine() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Luggage> tested = TList.of(
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8)
+        );
+        
+        Integer result = Te.e(KnapsackLine.solve(tested.map(l->p2i(l.weight(),l.value())),15)).get(15);
+        Integer expected = 20;
+        System.out.println("result  : "+result);
+        System.out.println("expected: "+expected);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testKnapsackLineLarge() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Luggage> tested = TList.of(
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8)
+        );
+        
+        int result = Te.e(KnapsackLine.solve(tested.map(l->p2i(l.weight(),l.value())),60)).get(60);
+        int expected = 95;
+        System.out.println("result  : "+result);
+        System.out.println("expected: "+expected);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testKnapsackLine0() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Luggage> tested = TList.of(
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8)
+        );
+        
+        Integer result = Te.e(KnapsackLine.solve0(tested.map(l->p2i(l.weight(),l.value())),15)).get(15);
+        Integer expected = 20;
+        System.out.println("result  : "+result);
+        System.out.println("expected: "+expected);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testKnapsackLine0Large() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Luggage> tested = TList.of(
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8),
+                new Luggage(11,15),
+                new Luggage(4,4),
+                new Luggage(2,3),
+                new Luggage(1,2),
+                new Luggage(3,1),
+                new Luggage(5,8)
+        );
+        
+        int result = Te.e(KnapsackLine.solve0(tested.map(l->p2i(l.weight(),l.value())),60)).get(60);
+        int expected = 95;
         System.out.println("result  : "+result);
         System.out.println("expected: "+expected);
         assertEquals(result, expected);
