5481b974a4a0fb39467f7949972c25efa2e94f65
masao <masao@Maverick>
2020-09-14 14:54:23 +0900

    tested accum-s.

--- src/collection/ArrayInt.java
+++ src/collection/ArrayInt.java
@@ -5,13 +5,20 @@
  */
 package collection;
 
-import debug.Te;
+import static collection.ArrayInt.ArrayIntIterator.concat;
+import static collection.ArrayInt.ArrayIntIterator.one;
+import iterator.Iterators;
+import java.util.AbstractList;
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.PrimitiveIterator;
-import java.util.function.Function;
+import java.util.function.IntBinaryOperator;
 import java.util.function.IntConsumer;
+import java.util.function.IntFunction;
 import java.util.function.IntPredicate;
 import java.util.function.IntUnaryOperator;
+import java.util.function.ToIntFunction;
+import java.util.stream.IntStream;
 
 /**
  *
@@ -21,30 +28,64 @@ public interface ArrayInt {
     static public ArrayInt arrayInt(int... body) {
         return new Plain(body);
     }
+    static public ArrayInt empty() {
+        return new Plain(new int[0]);
+    }
+    static public ArrayInt range(int from, int to) {
+        return new Range(from,to);
+    }
     public int get(int i);
     public int set(int i, int v);
     public int length();
+    default int last() {
+        return get(length()-1);
+    }
+    default int last(int i) {
+        return get(length()-1-i);
+    }
+    default ArrayInt cset(int i, int v) {
+        set(i,v);
+        return this;
+    }
+    default ArrayInt reset(ArrayInt other) {
+        int upto=Integer.min(length(),other.length());
+        for (int i=0;i<upto;i++) set(i,other.get(i));
+        return this;
+    }
+    default ArrayInt reset(ArrayInt other,int seek) {
+        this.seek(seek).reset(other); 
+        return this;
+    }
     default IntUnaryOperator asMap() {
         return i->get(i);
     }
     default ArrayInt index() {
-        return new Index(length());
+        return new Range(0,length());
     }
-    default PrimitiveIterator.OfInt iterator() {
-        return new IteratorInt(this);
+    default ArrayIntIterator iterator() {
+        return new PlainIterator(this);
     }
-    default PrimitiveIterator.OfInt fiterator(IntPredicate pred) {
-        return new FilterInt(this,pred);
+    default ArrayIntIterator FilterIter(IntPredicate pred) {
+        return new FilterIterator(this,pred);
     }
-    default ArrayInt asArray(Function<ArrayInt,PrimitiveIterator.OfInt> f) {
-        ArrayInt retval=new Plain(new int[length()]);
-        PrimitiveIterator.OfInt iter=f.apply(this);
-        int i=0;
-        for (i=0;iter.hasNext();i++) retval.set(i,iter.nextInt());
-        return retval.subArray(0, i);
+    default ArrayIntIterator accumWithSeedIter(int start, IntBinaryOperator op) {
+        return new AccumIterator(start,this,op);
+    }
+    default ArrayIntIterator accumIter(int start, IntBinaryOperator op) {
+        return one(start).append(accumWithSeedIter(start,op));
+    }
+    default ArrayIntIterator accumFromStartIter(IntUnaryOperator start, IntBinaryOperator op) {
+        if (length()==0) return ArrayIntIterator.empty();
+        return seek(1).accumIter(start.applyAsInt(get(0)), op);
+    }
+    static public ArrayIntIterator concatIter(ArrayIntIterator... body) {
+        return new ConcatIterator(body);
+    }
+    default IntStream stream() {
+        return Iterators.toIntStream(iterator());
     }
     default ArrayInt fix() {
-        return asArray(a->a.iterator());
+        return iterator().asArray();
     }
     default TList<Integer> asList() {
         return TList.set(i->ArrayInt.this.get(i),length());
@@ -55,11 +96,85 @@ public interface ArrayInt {
     default ArrayInt seek(int seek) {
         return seek>0?subArray(seek,length()):subArray(0,length()+seek);
     }
+    default ArrayInt reverse() {
+        return new Reverse(this);
+    }
+    default int sum() {
+        return stream().sum();
+    }
+    default ArrayInt pair(ArrayInt other, IntBinaryOperator op) {
+        return range(0,Integer.min(length(),other.length())).map(i->op.applyAsInt(get(i), other.get(i)));
+    }
+    default <T> TList<T> pairToObj(ArrayInt other, IntBiFunction<T> op) {
+        return range(0,Integer.min(length(),other.length())).mapToObj(i->op.apply(get(i), other.get(i)));
+    }
+    default ArrayInt diff(int seek,IntBinaryOperator op) {
+        return pair(seek(seek),op);
+    }
+    default ArrayInt diff(IntBinaryOperator op) {
+        return diff(1,op);
+    }
+    default <T> TList<T> diffToObj(int seek,IntBiFunction<T> op) {
+        return pairToObj(seek(seek),op);
+    }
+    default <T> TList<T> diffToObj(IntBiFunction<T> op) {
+        return diffToObj(1,op);
+    }
+    default TList<ArrayInt> reverseChunk(IntPredicate pred) {
+        ArrayInt divides=concat(one(0),FilterIter(pred),one(length())).asArray();
+        return divides.diffToObj((a,b)->subArray(a,b));
+    }
     default ArrayInt map(IntUnaryOperator op) {
         return new Map(this,op);
     }
+    default <T> TList<T> mapToObj(IntFunction<T> f) {
+        return TList.set(new AbstractList<T>() {
+            @Override
+            public T get(int index) {
+                return f.apply(ArrayInt.this.get(index));
+            }
+            @Override
+            public int size() {
+                return length();
+            }
+        });
+    }
+    default ArrayInt accumWithSeed(int start, IntBinaryOperator op) {
+        return accumWithSeedIter(start,op).asArray();
+    }
+    default ArrayInt accum(int start, IntBinaryOperator op) {
+        return accumIter(start,op).asArray();
+    }
+    default ArrayInt accumFromStart(IntUnaryOperator start, IntBinaryOperator op) {
+        return accumFromStartIter(start,op).asArray();
+    }
     default ArrayInt filter(IntPredicate pred) {
-        return asArray(a->a.fiterator(pred));
+        return FilterIter(pred).asArray();
+    }
+    default boolean forAll(IntPredicate pred) {
+        for (int i=0;i<length();i++)
+            if (!pred.test(i)) return false;
+        return true;
+    }
+    default boolean isUniform() {
+        return diff((a,b)->b-a).sum()==0;
+    }
+    default boolean isAscending() {
+        return diff((a,b)->b-a).forAll(i->i>0);
+    }
+    default boolean isDescending() {
+        return diff((a,b)->b-a).forAll(i->i<0);
+    }
+    default boolean isAscendingOrEqual() {
+        return diff((a,b)->b-a).forAll(i->i>=0);
+    }
+    default boolean isDescendingOrEqual() {
+        return diff((a,b)->b-a).forAll(i->i<=0);
+    }
+    default boolean exists(IntPredicate pred) {
+        for (int i=0;i<length();i++)
+            if (pred.test(i)) return true;
+        return false;
     }
     default void forEach(IntConsumer c) {
         for(int i=0;i<length();i++) c.accept(i);
@@ -84,6 +199,29 @@ public interface ArrayInt {
     }
     default int min(IntUnaryOperator op) {
         return min(0,length(),op);
+    }
+    default int max(int from, int to, IntUnaryOperator op) {
+        int max=op.applyAsInt(get(from));
+        int retval=0;
+        for (int i=from+1;i<to;i++) {
+            if (op.applyAsInt(get(i))>max) {
+                max=op.applyAsInt(get(i));
+                retval=i;
+            }
+        }
+        return retval;
+    }
+    default int max(int seek, IntUnaryOperator op) {
+        return seek>0?max(seek,length(),op):max(0,length()+seek,op);
+    }
+    default int max(IntUnaryOperator op) {
+        return max(0,length(),op);
+    }
+    default ArrayInt tee(IntConsumer c) {
+        return map(i->{
+            c.accept(i);
+            return i;
+        });
     }
     
     static class Plain implements ArrayInt {
@@ -130,6 +268,26 @@ public interface ArrayInt {
             return length;
         }
     }
+    static class Reverse implements ArrayInt {
+        ArrayInt body;
+        int length;
+        public Reverse(ArrayInt body) {
+            this.body=body;
+            this.length=body.length();
+        }
+        @Override
+        public int get(int i) {
+            return body.get(length-1-i);
+        }
+        @Override
+        public int set(int i, int v) {
+            return body.set(length-1-i, v);
+        }
+        @Override
+        public int length() {
+            return length;
+        }
+    }
     static class Map implements ArrayInt {
         ArrayInt body;
         IntUnaryOperator map;
@@ -150,14 +308,16 @@ public interface ArrayInt {
             return body.length();
         }
     }
-    static class Index implements ArrayInt {
+    static class Range implements ArrayInt {
+        int from;
         int length;
-        public Index(int length) {
-            this.length=length;
+        public Range(int from, int to) {
+            this.from=from;
+            this.length=to-from;
         }
         @Override
         public int get(int i) {
-            return i;
+            return i+from;
         }
         @Override
         public int set(int i, int v) {
@@ -168,12 +328,72 @@ public interface ArrayInt {
             return length;
         }
     }
-    static class IteratorInt implements PrimitiveIterator.OfInt {
+    @FunctionalInterface
+    interface IntBiFunction<T> {
+        T apply(int a,int b);
+    }
+    public static interface ArrayIntIterator extends PrimitiveIterator.OfInt {
+        int maxSize();
+        public static <T> ArrayIntIterator fromList(TList<T> list, ToIntFunction<T> f) {
+            return new ObjWrap<>(list,f);
+        }
+        public static ArrayIntIterator concat(TList<ArrayIntIterator> iters) {
+            return new ConcatIterator(iters);
+        }
+        public static ArrayIntIterator concat(ArrayIntIterator... iters) {
+            return new ConcatIterator(iters);
+        }
+        public static ArrayIntIterator one(int v) {
+            return new OneWrap(v);
+        }
+        public static ArrayIntIterator empty() {
+            return new EmptyIterator();
+        }
+        default int sum() {
+            int retval=0;
+            while(hasNext()) retval+=nextInt();
+            return retval;
+        }
+        default ArrayInt asArray() {
+            ArrayInt retval=new Plain(new int[maxSize()]);
+            int i=0;
+            for (i=0;hasNext();i++) retval.set(i,nextInt());
+            return retval.subArray(0, i);
+        }
+        default ArrayIntIterator map(IntUnaryOperator op) {
+            return new MapWrap(this,op);
+        }
+        default ArrayIntIterator filter(IntPredicate pred) {
+            return new FilterWrap(this,pred);
+        }
+        default ArrayIntIterator append(ArrayIntIterator... iters) {
+            return new ConcatIterator(TList.sof(iters).startFrom(this).sfix());
+        }
+    }
+    static class EmptyIterator implements ArrayIntIterator {
+        @Override
+        public int maxSize() {
+            return 0;
+        }
+        @Override
+        public int nextInt() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+    }
+    static class PlainIterator implements ArrayIntIterator {
         int i;
         ArrayInt body;
-        public IteratorInt(ArrayInt body) {
+        public PlainIterator(ArrayInt body) {
             this.i=0;
             this.body=body;
+        }
+        @Override
+        public int maxSize() {
+            return body.length();
         }
         @Override
         public int nextInt() {
@@ -185,21 +405,34 @@ public interface ArrayInt {
             return i<body.length();
         }
     }
-    static class FilterInt implements PrimitiveIterator.OfInt {
+    static class FilterIterator extends FilterAtIterator {
+        public FilterIterator(ArrayInt body,IntPredicate pred) {
+            super(body,pred);
+        }
+        @Override
+        public int nextInt() {
+            return body.get(super.nextInt());
+        }
+    }
+    static class FilterAtIterator implements ArrayIntIterator {
         int i;
         ArrayInt body;
         IntPredicate pred;
-        public FilterInt(ArrayInt body,IntPredicate pred) {
+        public FilterAtIterator(ArrayInt body,IntPredicate pred) {
             this.i=0;
             this.body=body;
             this.pred=pred;
         }
         @Override
+        public int maxSize() {
+            return body.length();
+        }
+        @Override
         public int nextInt() {
             if (hasNext())
-                return body.get(i++);
+                return i++;
             else
-                throw new NoSuchElementException("ArrayInt.FilterInt failed");
+                throw new NoSuchElementException("ArrayInt.FilterAtInt failed");
         }
         @Override
         public boolean hasNext() {
@@ -209,6 +442,176 @@ public interface ArrayInt {
             }
             return false;
         }
-        
+    }
+    static class AccumIterator implements ArrayIntIterator {
+        int i;
+        ArrayInt body;
+        IntBinaryOperator op;
+        int pre;
+        public AccumIterator(int start, ArrayInt body, IntBinaryOperator op) {
+            this.body=body;
+            this.op=op;
+            this.i=0;
+            this.pre=start;
+        }
+        @Override
+        public int maxSize() {
+            return body.length();
+        }
+        @Override
+        public int nextInt() {
+            if (!hasNext())
+                throw new NoSuchElementException("ArrayInt.AccumInt failed");
+            pre=op.applyAsInt(pre,body.get(i++));
+            return pre;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return i<body.length();
+        }
+    }
+
+    public class ConcatIterator implements ArrayIntIterator {
+        TList<ArrayIntIterator> iters;
+        int maxSize;
+        int current;
+        public ConcatIterator(TList<ArrayIntIterator> iters) {
+            this.iters=iters;
+            this.current=0;
+            this.maxSize=new ObjWrap<>(iters,i->i.maxSize()).sum();
+        }
+        public ConcatIterator(ArrayIntIterator... iters) {
+            this(TList.sof(iters));
+        }
+        @Override
+        public int maxSize() {
+            return maxSize;
+        }
+        @Override
+        public int nextInt() {
+            if (!hasNext())
+                throw new NoSuchElementException("ArrayInt.AccumInt failed");
+            return iters.get(current).nextInt();
+        }
+        @Override
+        public boolean hasNext() {
+            for (;current<iters.size();current++)
+                if (iters.get(current).hasNext())
+                    return true;
+            return false;
+        }
+    }
+    public class OneWrap implements ArrayIntIterator {
+        int v;
+        boolean hasNext;
+        public OneWrap(int v) {
+            this.v=v;
+            this.hasNext=true;
+        }
+        @Override
+        public int maxSize() {
+            return 1;
+        }
+        @Override
+        public int nextInt() {
+            if (!hasNext)
+                throw new NoSuchElementException("ArrayInt.OneWrap failed");
+            this.hasNext=false;
+            return v;
+        }
+        @Override
+        public boolean hasNext() {
+            return hasNext;
+        }
+    }
+    public class MapWrap implements ArrayIntIterator {
+        ArrayIntIterator body;
+        IntUnaryOperator op;
+        public MapWrap(ArrayIntIterator body,IntUnaryOperator op) {
+            this.body=body;
+            this.op=op;
+        }
+        @Override
+        public int maxSize() {
+            return body.maxSize();
+        }
+        @Override
+        public int nextInt() {
+            return op.applyAsInt(body.next());
+        }
+        @Override
+        public boolean hasNext() {
+            return body.hasNext();
+        }
+    }
+
+    class ObjWrap<T> implements ArrayIntIterator {
+        Iterator<T> body;
+        ToIntFunction<T> f;
+        int maxSize;
+        public ObjWrap(TList<T> body, ToIntFunction<T> f) {
+            this.body=body.iterator();
+            this.f=f;
+            this.maxSize=body.size();
+        }
+        @Override
+        public int maxSize() {
+            return maxSize;
+        }
+        @Override
+        public int nextInt() {
+            return f.applyAsInt(body.next());
+        }
+        @Override
+        public boolean hasNext() {
+            return body.hasNext();
+        }
+    }
+    abstract public class AbstractBufferedWrap implements ArrayIntIterator {
+        int buffer;
+        boolean has;
+        abstract protected void findNext();
+        public void nextFound(int buffered) {
+            this.buffer = buffered;
+            this.has = true;
+        }
+        public void prepare() {
+            if (!has)
+                findNext();
+        }
+        @Override
+        public boolean hasNext() {
+            prepare();
+            return has;
+        }
+        @Override
+        public int nextInt() {
+            prepare();
+            if (!has) 
+                throw new NoSuchElementException("AbstractBufferdWrap.nextInt() : gone beyond boundary (actual class is " + this.getClass().getName() + ")");
+            has = false;
+            return buffer;
+        }
+    }
+    class FilterWrap extends AbstractBufferedWrap {
+        ArrayIntIterator body;
+        IntPredicate pred;
+        public FilterWrap(ArrayIntIterator body, IntPredicate pred) {
+            this.body=body;
+            this.pred=pred;
+        }
+        @Override
+        protected void findNext() {
+            while (body.hasNext()) {
+                int i=body.nextInt();
+                if (pred.test(i))
+                    nextFound(i);
+            }
+        }
+        @Override
+        public int maxSize() {
+            return body.maxSize();
+        }
     }
 }
--- test/collection/ArrayIntNGTest.java
+++ test/collection/ArrayIntNGTest.java
@@ -5,6 +5,7 @@
  */
 package collection;
 
+import collection.ArrayInt.ArrayIntIterator;
 import static collection.ArrayInt.arrayInt;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
@@ -49,6 +50,86 @@ public class ArrayIntNGTest {
     }
 
     @Test
+    public void testSeek_minus() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Integer> result = arrayInt(0,1,2,3,4).seek(-1).asList();
+        TList<Integer> expected = TList.sofi(0,1,2,3);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+    
+    @Test
+    public void testReset() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Integer> result = arrayInt(0,1,2,3,4).reset(arrayInt(5,6,7)).asList();
+        TList<Integer> expected = TList.sofi(5,6,7,3,4);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+    
+    @Test
+    public void testReset2() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Integer> result = arrayInt(0,1,2,3,4).reset(arrayInt(5,6,7),1).asList();
+        TList<Integer> expected = TList.sofi(0,5,6,7,4);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+    
+    @Test
+    public void testPair() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Integer> result = arrayInt(0,1,2,3,4).pair(arrayInt(5,6,7),(a,b)->a+b).asList();
+        TList<Integer> expected = TList.sofi(5,7,9);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testDiff() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Integer> result = arrayInt(0,1,2,3,4).diff(1,(a,b)->b-a).asList();
+        TList<Integer> expected = TList.sofi(0,1,2,3,4).diff(1,(a,b)->b-a);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+    
+    @Test
+    public void testAccumWithSeed() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Integer> result = arrayInt(0,1,2,3,4).accumWithSeed(10,(a,b)->a-b).asList();
+        TList<Integer> expected = TList.sofi(0,1,2,3,4).accumWithSeed(10,(a,b)->a-b);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testAccum() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Integer> result = arrayInt(0,1,2,3,4).accum(10,(a,b)->a-b).asList();
+        TList<Integer> expected = TList.sof(0,1,2,3,4).accum(10,(a,b)->a-b);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    public void testAccumFromStart() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Integer> result = arrayInt(10,1,2,3,4).accumFromStart(a->a,(a,b)->a-b).asList();
+        TList<Integer> expected = TList.sof(10,1,2,3,4).accumFromStart(a->a,(a,b)->a-b);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+
+    @Test
     public void testMap() {
         System.out.println(test.TestUtils.methodName(0));
         TList<Integer> result = arrayInt(0,1,2,3,4).map(i->i+1).asList();
@@ -63,6 +144,27 @@ public class ArrayIntNGTest {
         System.out.println(test.TestUtils.methodName(0));
         TList<Integer> result = arrayInt(0,1,2,3,4).filter(i->i%2==0).asList();
         TList<Integer> expected = TList.sofi(0,2,4);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+    
+    @Test
+    public void testCiterator() {
+        System.out.println(test.TestUtils.methodName(0));
+        ArrayIntIterator iter=ArrayInt.concatIter(arrayInt(0,1,2).iterator(),arrayInt(3,4,5).iterator());
+        TList<Integer> result = iter.asArray().asList();
+        TList<Integer> expected = TList.sofi(0,1,2,3,4,5);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+    
+    @Test
+    public void testOne() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Integer> result = ArrayIntIterator.one(1).asArray().asList();
+        TList<Integer> expected = TList.sof(1);
         System.out.println("result  : " + result);
         System.out.println("expected: " + expected);
         assertEquals(result, expected);
