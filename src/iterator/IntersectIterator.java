1c8c5e02c06a97fe23962597ba23b944fd7b5c2d
masao <masao@Maverick>
2020-10-15 20:54:25 +0900

    added IntersectIterator.

---
+++ src/iterator/IntersectIterator.java
@@ -0,0 +1,55 @@
+/*
+ Copyright 2017, 2018 Masao Tomono
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package iterator;
+
+import java.util.Comparator;
+import java.util.Iterator;
+
+/**
+ *
+ * @author masao
+ * @param <T>
+ */
+public class IntersectIterator<T> extends AbstractBufferedIterator<T> {
+    BufferedIterator<T> left;
+    BufferedIterator<T> right;
+    Comparator<T> c;
+        
+    public IntersectIterator(Iterator<T> left, Iterator<T> right, Comparator<T> c) {
+        this.left = new BufferedIterator(left);
+        this.right = new BufferedIterator(right);
+        this.c = c;
+    }
+
+    @Override
+    protected void findNext() {
+        if (!left.hasNext()||!right.hasNext())
+            return;
+        left.next();right.next();
+        while(true) {
+            if (c.compare(left.peek(), right.peek())<0)
+                if (!left.hasNext()) return;
+                else left.next();
+            else if (c.compare(left.peek(), right.peek())>0)
+                if (!right.hasNext()) return;
+                else right.next();
+            else {
+                nextFound(left.peek());
+                return;
+            }
+        }
+   }
+}
---
+++ test/iterator/IntersectIteratorNGTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iterator;
+
+import collection.TList;
+import static collection.c.i2l;
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author masao
+ */
+public class IntersectIteratorNGTest {
+    
+    public IntersectIteratorNGTest() {
+    }
+
+    @Test
+    public void testIntersect0() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Integer> tested0=TList.sof(0,4,8,12,16,20);
+        TList<Integer> tested1=TList.sof(2,3,6,8,12,14,14,15,16,16,16,21);
+        TList<Integer> result = TList.set(i2l(new IntersectIterator<>(tested0.iterator(),tested1.iterator(),(a,b)->a-b)));
+        TList<Integer> expected = TList.sof(8,12,16);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+    @Test
+    public void testIntersect1() {
+        System.out.println(test.TestUtils.methodName(0));
+        TList<Integer> tested0=TList.sof(2,3,6,8,12,14,14,15,16,16,16,21);
+        TList<Integer> tested1=TList.sof(0,4,8,12,16,20);
+        TList<Integer> result = TList.set(i2l(new IntersectIterator<>(tested0.iterator(),tested1.iterator(),(a,b)->a-b)));
+        TList<Integer> expected = TList.sof(8,12,16);
+        System.out.println("result  : " + result);
+        System.out.println("expected: " + expected);
+        assertEquals(result, expected);
+    }
+    
+}
