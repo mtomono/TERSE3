58c44341c5232bf0ca2c91e01ccf7bad424dc0b3
masao <masao@Maverick>
2021-03-14 16:48:00 +0900

    now FranctionPowered is available

--- test/math/FractionPoweredNGTest.java
+++ test/math/FractionPoweredNGTest.java
@@ -20,7 +20,7 @@ public class FractionPoweredNGTest {
     @Test
     public void testSimple9800() {
         System.out.println(test.TestUtils.methodName(0));
-        FractionPowered result = FractionPowered.simple(9800,2);
+        FractionPowered result = FractionPowered.create(9800,2);
         FractionPowered expected = new FractionPowered(new Rational(70,1),2,new Rational(1,2));
         System.out.println("result  : " + result);
         System.out.println("expected: " + expected);
@@ -30,7 +30,7 @@ public class FractionPoweredNGTest {
     @Test
     public void testSimple16() {
         System.out.println(test.TestUtils.methodName(0));
-        FractionPowered result = FractionPowered.simple(16,3);
+        FractionPowered result = FractionPowered.create(16,3);
         FractionPowered expected = new FractionPowered(new Rational(2,1),2,new Rational(1,3));
         System.out.println("result  : " + result);
         System.out.println("expected: " + expected);
@@ -40,7 +40,7 @@ public class FractionPoweredNGTest {
     @Test
     public void testSimple625() {
         System.out.println(test.TestUtils.methodName(0));
-        FractionPowered result = FractionPowered.simple(625,3);
+        FractionPowered result = FractionPowered.create(625,3);
         FractionPowered expected = new FractionPowered(new Rational(5,1),5,new Rational(1,3));
         System.out.println("result  : " + result);
         System.out.println("expected: " + expected);
@@ -50,7 +50,7 @@ public class FractionPoweredNGTest {
     @Test
     public void testSimple32() {
         System.out.println(test.TestUtils.methodName(0));
-        FractionPowered result = FractionPowered.simple(32,3);
+        FractionPowered result = FractionPowered.create(32,3);
         FractionPowered expected = new FractionPowered(new Rational(2,1),2,new Rational(2,3));
         System.out.println("result  : " + result);
         System.out.println("expected: " + expected);
@@ -60,7 +60,7 @@ public class FractionPoweredNGTest {
     @Test
     public void testSimple64() {
         System.out.println(test.TestUtils.methodName(0));
-        FractionPowered result = FractionPowered.simple(64,3);
+        FractionPowered result = FractionPowered.create(64,3);
         FractionPowered expected = new FractionPowered(new Rational(4,1),1,new Rational(0,3));
         System.out.println("result  : " + result);
         System.out.println("expected: " + expected);
@@ -70,7 +70,7 @@ public class FractionPoweredNGTest {
     @Test
     public void testSimple32_81() {
         System.out.println(test.TestUtils.methodName(0));
-        FractionPowered result = FractionPowered.simple(32*81,3);
+        FractionPowered result = FractionPowered.create(32*81,3);
         FractionPowered expected = new FractionPowered(new Rational(6,1),12,new Rational(1,3));
         System.out.println("result  : " + result);
         System.out.println("expected: " + expected);
@@ -80,7 +80,7 @@ public class FractionPoweredNGTest {
     @Test
     public void testSimple32_243() {
         System.out.println(test.TestUtils.methodName(0));
-        FractionPowered result = FractionPowered.simple(32*243,3);
+        FractionPowered result = FractionPowered.create(32*243,3);
         FractionPowered expected = new FractionPowered(new Rational(6,1),6,new Rational(2,3));
         System.out.println("result  : " + result);
         System.out.println("expected: " + expected);
