ac685855a1bff6fb16dfae2827ca4aaf42e78102
masao <masao@Maverick>
2021-04-14 19:41:18 +0900

    fit to change in TERSE3

--- test/parser/Ipv6NGTest.java
+++ test/parser/Ipv6NGTest.java
@@ -6,8 +6,10 @@
 package parser;
 
 import collection.TList;
-import iterator.ReaderLinesIterator;
+import io.LinesReader;
+import iterator.TIterator;
 import java.io.File;
+import java.io.FileReader;
 import java.util.HashSet;
 import java.util.Set;
 import static java.util.stream.Collectors.toSet;
@@ -24,9 +26,10 @@ public class Ipv6NGTest {
     public Ipv6NGTest() {
     }
     @Test
-    public void testIpv6() {
+    public void testIpv6() throws Exception {
         System.out.println(test.TestUtils.methodName(0));
-        Set<String> result = ReaderLinesIterator.s(new File("chrome_debug20210408.log")).flatMap(l->new SimpleMatcher(ipv6d).extract(l)).collect(toSet());
+        FileReader reader=new FileReader(new File("chrome_debug20210408.log"));
+        Set<String> result = new LinesReader(reader).stream().flatMap(l->new SimpleMatcher(ipv6d).extract(l)).collect(toSet());
         Set<String> expected = new HashSet<>(TList.sof("::","::0","::1:0","::0:Eb8e","::0:0:0","240d:1a:fd:x:x:x:x:x","2001:4451:45a2:x:x:x:x:x"));
         System.out.println("result  : " + result);
         System.out.println("expected: " + expected);
