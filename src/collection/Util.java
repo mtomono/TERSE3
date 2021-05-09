2748d8df3b0e7c5ba8daec561c24dbfcf2398e3b
masao <masao@Maverick>
2021-05-09 20:13:06 +0900

    added operations for Set

---
+++ src/collection/Util.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collection;
+
+import java.util.Set;
+
+/**
+ *
+ * @author masao
+ */
+public class Util {
+    public static <T> Set<T> add(Set<T> one, Set<T> two) {
+        one.addAll(two);
+        return one;
+    }
+    public static <T> Set<T> sub(Set<T> one, Set<T> two) {
+        one.removeAll(two);
+        return one;
+    }
+}
